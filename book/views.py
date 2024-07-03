b72d2fae8485e0c6853bea01781fd807f155a4c9
Zoirjon <jalilov@mail.com>
2024-07-03 06:32:10 +0500

    lesson 3

---
+++ book/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Book
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def books(request):
+    books = Book.objects.all
+    context = {'books': books}
+    return render(request, 'books.html', context=context)
